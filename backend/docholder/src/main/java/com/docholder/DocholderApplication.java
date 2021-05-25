ea157f6bd25e068ac4c2f51a18595545a7d3f17d
unknown <vladislav.ugm@gmail.com>
2021-05-25 22:31:59 +0300

    some fixes

--- backend/docholder/src/main/java/com/docholder/DocholderApplication.java
+++ backend/docholder/src/main/java/com/docholder/DocholderApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DocholderApplication {
